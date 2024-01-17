1a28af8ad812274e10926fd3be803408eede57ee
SANJAYJY <sanjay.jayaseelan@multicorewareinc.com>
2024-01-17 12:40:55 +0530

    Basic Cuda programs

--- cuda/add.cpp
+++ cuda/add.cpp
@@ -11,24 +11,18 @@ void addVectors(const std::vector<int>& a, const std::vector<int>& b, std::vecto
 }
 
 int main() {
-    // Host vectors
     std::vector<int> h_a(N, 1);
     std::vector<int> h_b(N, 2);
     std::vector<int> h_c(N);
 
-    // Measure the time for CPU computation
     auto start_time = std::chrono::high_resolution_clock::now();
 
-    // Perform vector addition on the CPU
     addVectors(h_a, h_b, h_c);
 
     auto end_time = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
 
     std::cout <<duration.count() << "\n";
-
-    // Print the result or further process the result as needed
-    // ...
 
     return 0;
 }
